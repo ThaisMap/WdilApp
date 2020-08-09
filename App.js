b481480879d64b2a5237ac99ded09850aad3ebaa
Hudson <hudson.mendonca.almeida@gmail.com>
2020-08-09 18:54:00 -0300

    commitei

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import 'react-native-gesture-handler';
 import auth from '@react-native-firebase/auth';
 import React, { useState, useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+  statusCodes,
+} from '@react-native-community/google-signin';
 
 import Routes from './src/Routes';
 
 export default function App() {
+  GoogleSignin.configure({
+    webClientId:
+      '819861619036-4dc4d9fhb0i85ggp32lqkeqlbtpav9ee.apps.googleusercontent.com',
+  });
+
   // Set an initializing state whilst Firebase connects
   const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState();
@@ -16,6 +26,47 @@ export default function App() {
     if (initializing) {
       setInitializing(false);
     }
+  }
+
+  function GoogleSignIn() {
+    return (
+      <Button
+        title="Google Sign-In"
+        onPress={() =>
+          onGoogleButtonPress().then(() =>
+            console.log('Signed in with Google!'),
+          )
+        }
+      />
+    );
+  }
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+      } else if (error.code === statusCodes.IN_PROGRESS) {
+        // operation (e.g. sign in) is in progress already
+      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        // play services not available or outdated
+      } else {
+        // some other error happened
+      }
+    }
+  };
+
+  async function onGoogleButtonPress() {
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
   }
 
   useEffect(() => {
@@ -29,12 +80,12 @@ export default function App() {
   }
 
   if (!user) {
-    return (
-      <View>
-        <Text> Login </Text>{' '}
-      </View>
-    );
+    GoogleSignIn();
   }
 
-  return <Routes />;
+  return (
+    <View>
+      <Text>Welcome {user.email}</Text>
+    </View>
+  );
 }
